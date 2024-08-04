81b492cac04b3fbaabc75ce8c56a62270f390fc6
Dipak-Gautam <anjangautam095@gmail.com>
2024-08-04 08:41:37 +0545

    initilization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <p className="text-2xl text-orange-400">Hello projeect start from here</p>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
+
+export default App;
