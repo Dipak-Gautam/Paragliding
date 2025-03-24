12457e2951f39db42bc10083a33351d5c4ba8b27
Dipak-Gautam <anjangautam095@gmail.com>
2025-03-24 15:14:01 +0545

    feat: navigation done for mobile version

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import OffCanvas from "./OffCanvas/OffCanvas";
 
 const Navbar = () => {
   return (
     <div className="bg-[#003446]">
-      <div className="flex justify-between p-2  md:mx-20">
+      <div className="flex justify-between p-2 mx-3 md:mx-20">
         <div className="rounded-md overflow-hidden">
-          <img src="/Logo/skyView.png" style={{ height: 70, width: 200 }} />
+          <img src="/Logo/skyView.png" className="h-20" />
         </div>
-        <div className="flex my-auto space-x-8 text-lg font-semibold text-white ">
+        <div className="hidden md:flex my-auto space-x-8 text-lg font-semibold text-white ">
           <Link className="no-underline" to="/">
             <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
               Home
@@ -35,8 +36,10 @@ const Navbar = () => {
             </div>
           </Link>
         </div>
+        <div className=" flex md:hidden justify-center items-center">
+          <OffCanvas />
+        </div>
       </div>
-      <OffCanvas />
     </div>
   );
 };
--- src/Components/Navbar/OffCanvas/OffCanvas.tsx
+++ src/Components/Navbar/OffCanvas/OffCanvas.tsx
@@ -1,5 +1,8 @@
 import Offcanvas from "react-bootstrap/Offcanvas";
 import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { IoCloseSharp } from "react-icons/io5";
 
 const OffCanvas = () => {
   const [show, setShow] = useState(false);
@@ -9,14 +12,61 @@ const OffCanvas = () => {
 
   return (
     <>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-        </Offcanvas.Body>
+      <div>
+        <GiHamburgerMenu
+          className="text-white"
+          size={25}
+          onClick={() => handleShow()}
+        />
+      </div>
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        style={{
+          width: 300,
+        }}
+      >
+        <div className="bg-[#003446] w-full h-full p-3">
+          <div className="flex justify-between items-center pb-4 mb-4 border-b border-white">
+            <div className="rounded-md overflow-hidden">
+              <img src="/Logo/skyView.png" className="h-14" />
+            </div>
+            <div>
+              <IoCloseSharp
+                className="text-white text-3xl"
+                onClick={handleClose}
+              />
+            </div>
+          </div>
+
+          <div className=" my-auto space-x-8 text-sm font-semibold text-white ">
+            <Link className="no-underline" to="/">
+              <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+                Home
+              </div>
+            </Link>
+            <Link className="no-underline" to="/blog">
+              <div className=" text-slate-50 hover:underline hover:text-yellow-400 hover:-translate-y-1 cursor-pointer ">
+                Blog
+              </div>
+            </Link>
+            <Link className="no-underline" to="/about-us">
+              <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+                About Us
+              </div>
+            </Link>
+            <Link className="no-underline" to="/gallery">
+              <div className=" text-slate-50  hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+                Gallery
+              </div>
+            </Link>
+            <Link className="no-underline" to="/contact">
+              <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+                Contact us
+              </div>
+            </Link>
+          </div>
+        </div>
       </Offcanvas>
     </>
   );
