1be133153dea1d2117db68f93da432018c820b98
Dipak-Gautam <anjangautam095@gmail.com>
2025-03-26 15:45:28 +0545

    feat: hero image and about us made responsive

--- src/Components/AboutUs/HeroAbout.jsx
+++ src/Components/AboutUs/HeroAbout.jsx
@@ -5,7 +5,7 @@ const HeroAbout = ({ style }) => {
   return (
     <div className=" p-3 py-5 md:mx-20">
       <div className="flex  space-x-4">
-        <div>
+        <div className="hidden md:flex">
           <img
             src="AboutUs/about-us.png"
             alt=""
@@ -14,10 +14,10 @@ const HeroAbout = ({ style }) => {
         </div>
         <div className="py-2 md:w-[50%] my-auto space-y-4">
           <div className="text-blue-600 font-semibold">About Us</div>
-          <div className="text-xl md:text-4xl font-bold">
+          <div className="text-lg md:text-4xl font-bold">
             We Have 15 Years of Experience in Paragliding & Hiking
           </div>
-          <div className="text-justify">
+          <div className="text-justify text-sm md:text-base">
             Our's Paragliding is a family run company and has been offering
             tandem flights in the Annapurna region of Nepal since 1996. As the
             most experienced paragliding company in Nepal, we are unrivaled in
@@ -28,8 +28,8 @@ const HeroAbout = ({ style }) => {
             year.
           </div>
           <div>
-            <p className="font-bold text-xl m-0">Our Services</p>
-            <p>
+            <p className="font-bold text-lg md:text-xl m-0">Our Services</p>
+            <p className="text-sm md:text-base">
               <li>Tandem Flights</li>
               <li>Hiking</li>
               <li>Tracking</li>
@@ -42,7 +42,7 @@ const HeroAbout = ({ style }) => {
               <Link to="/about-us">
                 <button
                   type="button"
-                  class=" bg-green-900 text-white p-2 px-3 font-semibold font-serif hover:bg-green-700 duration-500"
+                  class=" bg-green-900 text-white p-2 px-3 rounded-md font-semibold font-serif hover:bg-green-700 duration-500 text-sm ms:text-base"
                 >
                   More About Us
                 </button>
--- src/Components/HeroImage/HeroImage.jsx
+++ src/Components/HeroImage/HeroImage.jsx
@@ -13,40 +13,40 @@ const HeroImage = () => {
         style={{
           backgroundImage: `url(${bgImage})`,
         }}
-        className=" bg-no-repeat bg-center bg-cover w-full h-[70vh] flex flex-col justify-center items-end relative"
+        className=" bg-no-repeat bg-center bg-cover w-full h-[50vh] md:h-[70vh] flex flex-col justify-center items-end relative"
       >
-        <div className=" mx-14">
-          <p
-            className="text-white text-xs font-bold  "
+        <div className=" mx-14 space-y-2">
+          <div
+            className="text-white text-xs ms:text-xs font-bold  "
             data-aos="fade-out"
             data-aos-duration="1000"
           >
             We are SkyView,
-          </p>
-          <p
-            className="text-white text-4xl font-extrabold "
+          </div>
+          <div
+            className="text-white text-2xl md:text-4xl font-extrabold "
             data-aos="fade-up"
             data-aos-delay="500"
             data-aos-duration="1000"
           >
             Would You Like To Join Our
-          </p>
-          <p
-            className="text-white text-4xl font-extrabold "
+          </div>
+          <div
+            className="text-white  text-2xl md:text-4xl font-extrabold "
             data-aos="fade-up"
             data-aos-delay="1000"
             data-aos-duration="1000"
           >
             Paragliding Adventure
-          </p>
-          <p
+          </div>
+          <div
             className="text-white text-xs font-bold  "
             data-aos="fade-up"
             data-aos-duration="1000"
             data-aos-delay="1500"
           >
             Pokhara, Nepal
-          </p>
+          </div>
         </div>
         {/* <div className="w-full absolute bottom-[5%]">
           <AnimatedCounter />
