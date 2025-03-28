bdba36a6f81ae934a458c882eedf5d23cc630a33
Dipak-Gautam <anjangautam095@gmail.com>
2025-03-28 15:42:29 +0545

    chore : Booking bug fixed

--- src/Components/BookNow/BookNow.jsx
+++ src/Components/BookNow/BookNow.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import bgimage from "./bg/bookbg.jpg";
+import bgImage from "./bg/book-bg.jpg";
 
 const BookNow = () => {
   return (
     <div className="p-3 md:px-20">
       <div
-        style={{ backgroundImage: `url(${bgimage})` }}
-        className="bg-no-repeat bg-cover bg-center rounded-3xl p-8 flex justify-between"
+        style={{ backgroundImage: `url(${bgImage})` }}
+        className="bg-no-repeat bg-cover bg-center rounded-3xl p-8 flex justify-between gap-4"
       >
         <div className="text-center flex-1">
-          <p className="text-3xl text-white font-extrabold ">
+          <p className=" text-xl md:text-3xl text-white font-extrabold ">
             Book your seat today for paragliding.
           </p>
-          <p className="text-white">Get the best deals right now.</p>
+          <p className="text-white text-sm md:text-base">
+            Get the best deals right now.
+          </p>
         </div>
         <div className="font-bold my-auto">
-          <p className="bg-slate-50 p-3 px-4 text-black text-2xl rounded-lg cursor-pointer hover:bg-red-400  duration-700 hover:bg-gradient-to-r hover:from-purple-500 hover:to-pink-500 ">
+          <p className="bg-slate-50 p-3 md:px-4 text-black text-lg md:text-2xl rounded-lg cursor-pointer hover:bg-red-400  duration-700 hover:bg-gradient-to-r hover:from-purple-500 hover:to-pink-500 ">
             Book Now
           </p>
         </div>
