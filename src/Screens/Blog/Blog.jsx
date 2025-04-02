2816e9c57c34838f2cad069bd57c9fe4ea07f24e
Dipak-Gautam <anjangautam095@gmail.com>
2025-04-02 21:18:50 +0545

    chore : gallery error fixed

--- src/Screens/Blog/Blog.jsx
+++ src/Screens/Blog/Blog.jsx
@@ -5,10 +5,10 @@ const Blog = () => {
   return (
     <div className="p-3 md:mx-20 h-[45vh] flex items-center justify-center">
       <div className="flex">
-        <div className="text-red-500 text-9xl">
+        <div className="text-red-500 text-5xl md:text-9xl">
           <CgSmileSad />
         </div>
-        <div className="my-auto mx-3 text-2xl  font-bold">
+        <div className="my-auto mx-3 text-base md:text-2xl  font-bold">
           Sorry there are no Blogs to show
         </div>
       </div>
--- src/Screens/Gallery/Gallery.jsx
+++ src/Screens/Gallery/Gallery.jsx
@@ -5,15 +5,15 @@ const Gallery = () => {
     <div className="p-3 md:mx-20">
       <div>
         <h1 className="my-3">Our Gallery</h1>
-        <div className="flex justify-between my-4 ">
-          <div className="w-[41%] rounded-3xl overflow-hidden ">
+        <div className="md:flex justify-between my-4 space-y-4">
+          <div className="md:w-[41%] rounded-3xl overflow-hidden ">
             <img
               src="Gallery/row11.jpg"
               alt=""
               style={{ width: 600, height: 450 }}
             />
           </div>
-          <div className="w-[57%] rounded-3xl overflow-hidden border ">
+          <div className="md:w-[57%] rounded-3xl overflow-hidden border ">
             <img
               src="Gallery/row12.jpg"
               alt=""
@@ -21,15 +21,15 @@ const Gallery = () => {
             />
           </div>
         </div>
-        <div className="flex justify-between my-4">
-          <div className="w-[57%] rounded-3xl overflow-hidden border ">
+        <div className="md:flex justify-between my-4 space-y-4">
+          <div className="md:w-[57%] rounded-3xl overflow-hidden border ">
             <img
               src="Gallery/row21.jpg"
               alt=""
               style={{ height: 450, width: 800 }}
             />
           </div>
-          <div className="w-[41%] rounded-3xl overflow-hidden ">
+          <div className="md:w-[41%] rounded-3xl overflow-hidden ">
             <img
               src="Gallery/row22.jpg"
               alt=""
@@ -37,15 +37,15 @@ const Gallery = () => {
             />
           </div>
         </div>
-        <div className="flex justify-between my-4 ">
-          <div className="w-[41%] rounded-3xl overflow-hidden ">
+        <div className="md:flex justify-between my-4 space-y-4">
+          <div className="md:w-[41%] rounded-3xl overflow-hidden ">
             <img
               src="Gallery/row31.jpg"
               alt=""
               style={{ width: 600, height: 450 }}
             />
           </div>
-          <div className="w-[57%] rounded-3xl overflow-hidden border ">
+          <div className="md:w-[57%] rounded-3xl overflow-hidden border ">
             <img
               src="Gallery/row32.jpg"
               alt=""
@@ -53,15 +53,15 @@ const Gallery = () => {
             />
           </div>
         </div>
-        <div className="flex justify-between my-4">
-          <div className="w-[57%] rounded-3xl overflow-hidden border ">
+        <div className="md:flex justify-between my-4 space-y-4">
+          <div className="md:w-[57%] rounded-3xl overflow-hidden border ">
             <img
               src="Gallery/row41.jpg"
               alt=""
               style={{ height: 450, width: 800 }}
             />
           </div>
-          <div className="w-[41%] rounded-3xl overflow-hidden ">
+          <div className="md:w-[41%] rounded-3xl overflow-hidden ">
             <img
               src="Gallery/row42.jpg"
               alt=""
@@ -69,15 +69,15 @@ const Gallery = () => {
             />
           </div>
         </div>
-        <div className="flex justify-between my-4 ">
-          <div className="w-[41%] rounded-3xl overflow-hidden ">
+        <div className="md:flex justify-between my-4 space-y-4">
+          <div className="md:w-[41%] rounded-3xl overflow-hidden ">
             <img
               src="Gallery/row51.jpg"
               alt=""
               style={{ width: 600, height: 450 }}
             />
           </div>
-          <div className="w-[57%] rounded-3xl overflow-hidden border ">
+          <div className="md:w-[57%] rounded-3xl overflow-hidden border ">
             <img
               src="Gallery/row52.jpg"
               alt=""
