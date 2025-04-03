81d0759ca28ca85fc110418f4a8f4575be0e5fa8
Dipak-Gautam <anjangautam095@gmail.com>
2025-04-03 21:29:19 +0545

    feat: navLink active bug fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,32 +6,67 @@ const Navbar = () => {
   return (
     <div className="bg-[#003446]">
       <div className="flex justify-between p-2 mx-3 md:mx-20">
-        <div className="rounded-md overflow-hidden">
+        <NavLink to="/" className="rounded-md overflow-hidden">
           <img src="/Logo/skyView.png" className="h-20" />
-        </div>
-        <div className="hidden md:flex my-auto space-x-8 text-lg font-semibold text-white ">
-          <NavLink className="no-underline" to="/">
-            <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+        </NavLink>
+        <div className="hidden md:flex my-auto space-x-8 text-lg font-semibold text-white no-underline">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "text-slate-400 no-underline"
+                : " text-slate-50  no-underline"
+            }
+          >
+            <div className="  hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer ">
               Home
             </div>
           </NavLink>
-          <NavLink className="no-underline" to="/blog">
-            <div className=" text-slate-50 hover:underline hover:text-yellow-400 hover:-translate-y-1 cursor-pointer ">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-slate-400 no-underline"
+                : " text-slate-50  no-underline"
+            }
+            to="/blog"
+          >
+            <div className="  hover:underline hover:text-yellow-400 hover:-translate-y-1 cursor-pointer ">
               Blog
             </div>
           </NavLink>
-          <NavLink className="no-underline" to="/about-us">
-            <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-slate-400 no-underline"
+                : " text-slate-50  no-underline"
+            }
+            to="/about-us"
+          >
+            <div className="  hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
               About Us
             </div>
           </NavLink>
-          <NavLink className="no-underline" to="/gallery">
-            <div className=" text-slate-50  hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-slate-400 no-underline"
+                : " text-slate-50  no-underline"
+            }
+            to="/gallery"
+          >
+            <div className="   hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
               Gallery
             </div>
           </NavLink>
-          <NavLink className="no-underline" to="/contact">
-            <div className=" text-slate-50 hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-slate-400 no-underline"
+                : " text-slate-50  no-underline"
+            }
+            to="/contact"
+          >
+            <div className=" hover:underline hover:text-yellow-400  hover:-translate-y-1 cursor-pointer">
               Contact Us
             </div>
           </NavLink>
